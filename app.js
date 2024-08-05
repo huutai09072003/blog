77cbd85d6f7c69ba0cc16709b15778e402efd8b8
Huu Tai <tain6507@gmail.com>
2024-08-05 14:59:58 +0700

    static express update

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express = require("express")
+var config = require("config")
+var bodyParser = require("body-parser")
 
 var app= express();
 
-var controllers = require(__dirmane + "./apps/controllers")
+app.set("views", __dirname + "/apps/views");
+app.set("view engine", "ejs");
+
+app.use("/static", express.static(__dirname + "/public"));
+
+var controllers = require(__dirname + "/apps/controllers")
 
 app.use(controllers);
+app.use(bodyParser.json());
 
-app.listen(3000, function(){
-    console.log("Server is running on port ", 3000);
+
+var host = config.get("server.host")
+var port = config.get("server.port")
+
+app.listen(port, host, function(){
+    console.log("Server is running on port ", port);
 });
--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -2,11 +2,12 @@ var express = require("express")
 
 var router = express.Router();
 
-router.use("admin", require(__dirname+ "./admin"));
-router.use("blog", require(__dirname+ "./blog"));
+router.use("admin", require(__dirname + "/admin"));
+router.use("blog", require(__dirname + "/blog"));
 
 router.get("/", function(req, res){
-    res.json({"message":"This is home page"})
+    // res.json({"message":"This is home page"})
+    res.render("test")
 });
 
 module.exports = router;
